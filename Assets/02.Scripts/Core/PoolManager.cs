6171d43a19dca64fa637d366a84d3ff28d619246
ctmanjak <ctmanjak@gmail.com>
2025-12-10 14:39:31 +0900

    feat: 오브젝트 풀링 추가

---
+++ Assets/02.Scripts/Core/PoolManager.cs
@@ -0,0 +1,69 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Pool;
+
+namespace Core
+{
+    public class PoolManager: MonoBehaviour
+    {
+        private static PoolManager _instance;
+
+        [SerializeField] private GameObject[] _objects;
+        
+        private static Dictionary<GameObject, ObjectPool<GameObject>> _pools = new();
+        private static Dictionary<GameObject, GameObject> _prefabs = new();
+
+        private void Awake()
+        {
+            if (_instance != null)
+            {
+                Destroy(gameObject);
+                return;
+            }
+            
+            _instance = this;
+        }
+        private void Start()
+        {
+            foreach (var obj in _objects)
+            {
+                _pools[obj] = new ObjectPool<GameObject>(
+                    () =>
+                    {
+                        GameObject instantiated = Instantiate(obj);
+                        instantiated.SetActive(false);
+                        _prefabs[instantiated] = obj;
+                        return instantiated;
+                    },
+                    (o) =>
+                    {
+                        o.SetActive(true);
+                    },
+                    (o) =>
+                    {
+                        o.SetActive(false);
+                    },
+                    (o) =>
+                    {
+                        _prefabs.Remove(o);
+                        Destroy(o);
+                    }
+                );
+            }
+        }
+
+        public static GameObject Get(GameObject prefab)
+        {
+            GameObject obj = _pools[prefab].Get();
+            _prefabs[obj] = prefab;
+            return obj;
+        }
+
+        public static void Release(GameObject obj)
+        {
+            GameObject prefab = _prefabs[obj];
+            _pools[prefab].Release(obj);
+            _prefabs.Remove(obj);
+        }
+    }
+}
--- Assets/02.Scripts/Player/PlayerBomb.cs
+++ Assets/02.Scripts/Player/PlayerBomb.cs
@@ -1,3 +1,4 @@
+using Core;
 using UnityEngine;
 
 namespace Player
@@ -5,12 +6,13 @@ namespace Player
     public class PlayerBomb : MonoBehaviour
     {
         [SerializeField] private Transform _fireTransform;
-        [SerializeField] private Bomb _bombPrefab;
+        [SerializeField] private GameObject _bombPrefab;
         [SerializeField] private float _throwPower;
 
         public void Fire()
         {
-            Bomb bomb = Instantiate(_bombPrefab, _fireTransform.position, Quaternion.identity);
+            GameObject bomb = PoolManager.Get(_bombPrefab);
+            bomb.transform.position = _fireTransform.position;
             Rigidbody bombBody = bomb.GetComponent<Rigidbody>();
             bombBody.AddForce(Camera.main.transform.forward * _throwPower, ForceMode.Impulse);
         }
